ff3d18d7e96dfa205f321a0a6427f4ccdadf5a21
Morgan <morganokeke@gmail.com>
2022-10-14 07:15:43 -0700

    prints the alphabet in lowercase, except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main -  prints the alphabet in lowercase except q and e
+* Return: 0 is returned
+*/
+
+int main(void)
+{
+	int m, e, q;
+	e = 'e';
+	q = 'q';
+	/*@for prints the alphabet in lowercase except q and e*/
+	for (m = 'a'; m <= 'z'; m++)
+	{
+		/*@if checks for q and e and prints others*/
+		if (m != e && m != q)
+			putchar(m);
+	}
+	putchar('\n');
+	return (0);
+
+}
