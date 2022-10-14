a211090e8b5f7270a8d163b7c1b372f7156dd1db
Morgan <morganokeke@gmail.com>
2022-10-13 17:28:52 -0700

    C program that prints exactly a sentence

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+* main - C program that prints a sentence
+* Return: 1 is returned
+*/
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);	
+}
