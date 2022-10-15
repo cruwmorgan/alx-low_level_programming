9def744d493d7d8c7ba6a21d215c23a58e3b3bca
Morgan <morganokeke@gmail.com>
2022-10-14 21:10:57 -0700

    prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,26 +3,26 @@
 #include <time.h>
 
 /**
-* main -  prints all possible combinations of single-digit numbers
+* main - prints all possible different combinations of two digits
 * Return: 0 is returned
 */
 int main(void)
 {
 	int m, n;
-	/*@for loops through 0 to 8*/
+	/*@for loops through 0 to 9*/
 	for (m = '0'; m < '9' ; m++)
 	{
-		/*@for loops through 0 to 9*/
-		for (n = m + 1; n <= 9; n++)
+		/*@for loops from 0 to 8*/
+		for (n = m + 1; n <= '9'; n++)
 		{
-			/*@if checks if n and m are similar*/
+			/*@if checks n to m*/
 			if (n != m)
 			{
 				putchar(m);
 				putchar(n);
-				/*@if checks if n is not 9 and m not 8*/
-				if (n != '9' && m != '8')
-					continue;
+				/*@if checks m to 8 and n to 9*/
+				if (m == '8' && n == '9')
+					continue;		
 				putchar(',');
 				putchar(' ');
 			}
