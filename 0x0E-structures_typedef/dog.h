49145ec22eb0c41c482d1755ac1742377b2df4da
Morgan <morganokeke@gmail.com>
2022-11-14 19:55:25 -0800

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+* struct dog -structure for a dog
+* @name: name of the dog
+* @age: age of the dog
+* @owner: owner of the dog
+* Description: new type struct dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*_DOG_H_*/
